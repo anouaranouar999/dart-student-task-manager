0970ad368e79747eba0f811e04e64abc0046a9f0
Anouar Segdoud <anouarsegdoud400@gmail.com>
2025-12-21 11:49:09 +0100

    Add readString helper function with proper input validation

--- helpers.dart
+++ helpers.dart
@@ -1,10 +1,30 @@
 import 'dart:io';
 
 readInt(prompt) {
-  try {
+  while (true) {
+    print(prompt);
     stdout.write("enter a number :");
     String? number = stdin.readLineSync();
-  } catch (e) {
-    print(e);
+    var numb = num.tryParse(number!);
+    if (numb == null) {
+      print("Invalid input");
+      continue;
+    } else {
+      return numb;
+    }
+  }
+}
+
+readString(prompt) {
+  while (true) {
+    stdout.write(prompt);
+    String? input = stdin.readLineSync();
+    if (input == null) {
+      continue;
+    } else if (input.isEmpty) {
+      continue;
+    } else {
+      return input;
+    }
   }
 }
